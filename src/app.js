c496402343567198d07029662c321134b22072ab
Jack Franklin <jack@jackfranklin.net>
2016-03-02 10:49:55 +0000

    Remove most of the example code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,1 @@
-import { text } from './text';
-
-var heading = document.createElement('h1');
-heading.appendChild(document.createTextNode('Hello World'));
-
-document.body.appendChild(heading);
-
+console.log('hello world');
--- src/text.js
+++
@@ -1,3 +0,0 @@
-const text = 'Hello World';
-
-export { text };
